51683ce88caf1ec00a2d78c293109b767b3a4bcf
azim0nt <kktotov457@gmail.com>
2024-08-27 03:02:02 +0500

    test styles

---
+++ src/components/Test/index.jsx
@@ -0,0 +1,17 @@
+import './style.scss'
+
+function Test() {
+    return (
+        <>
+            <div className="test-wrapper">
+                <div className="test-content">
+                    <h1>This is Test</h1>
+                    <a href="https://youtu.be/dQw4w9WgXcQ?si=oH-CndaIq-3l8si3">Free Money</a>
+                    <button>Test Button</button>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Test;
