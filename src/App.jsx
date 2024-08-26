d1a52c9424aee98a08f5c3e21f43df6ad75c4cbd
azim0nt <kktotov457@gmail.com>
2024-08-27 03:04:43 +0500

    add test to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { context, globalReducer, initialState } from './store'
 import { useReducer } from 'react'
-
+import Test from './components/Test'
 function App() {
 
   const [state, dispatch] = useReducer(globalReducer, initialState)
@@ -12,7 +12,7 @@ function App() {
     <>
       <context.Provider value={contextPayload}>
         <div>
-
+      <Test/>
         </div>
       </context.Provider>
     </>
