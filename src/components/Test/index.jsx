83c665c87d2116d96ab7d979856d2658d37ab85c
azim0nt <kktotov457@gmail.com>
2024-08-27 14:21:12 +0500

    add open link

--- src/components/Test/index.jsx
+++ src/components/Test/index.jsx
@@ -1,12 +1,20 @@
 import './style.scss'
-
+import { useContext } from 'react';
+import { context } from '../../store';
 function Test() {
+    const { store } = useContext(context);
+    function openLinkFn(link){
+        const option = {
+            target:'_blank'
+        }
+        store.tg.openLink(link, option);
+    }
     return (
         <>
             <div className="test-wrapper">
                 <div className="test-content">
                     <h1>This is Test</h1>
-                    <a href="https://youtu.be/dQw4w9WgXcQ?si=oH-CndaIq-3l8si3">Free Money</a>
+                    <button onClick={openLinkFn} className='link-btn' href="https://youtu.be/dQw4w9WgXcQ?si=oH-CndaIq-3l8si3">Free Money</button>
                     <button>Test Button</button>
                 </div>
             </div>
