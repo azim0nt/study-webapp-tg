6b054ddf87a18af2df448e7ab002856e11f0f18c
azim0nt <kktotov457@gmail.com>
2024-08-27 15:01:49 +0500

    study main button

--- src/components/Test/index.jsx
+++ src/components/Test/index.jsx
@@ -3,19 +3,26 @@ import { useContext } from 'react';
 import { context } from '../../store';
 function Test() {
     const { store } = useContext(context);
-    function openLinkFn(link){
+    function openLinkFn(link) {
         const option = {
-            target:'_blank'
+            target: '_blank'
         }
         store.tg.openLink(link, option);
     }
+    function showMainBtnFn(str){
+        store.tg.MainButton.show()
+        store.tg.MainButton.setText(str)
+        
+    }
+    store.tg.onEvent('mainButtonClicked', ()=>{store.tg.close()})
     return (
         <>
             <div className="test-wrapper">
                 <div className="test-content">
                     <h1>This is Test</h1>
-                    <button onClick={() =>{openLinkFn('https://youtu.be/dQw4w9WgXcQ?si=oH-CndaIq-3l8si3')}} className='link-btn' href="https://youtu.be/dQw4w9WgXcQ?si=oH-CndaIq-3l8si3">Free Money</button>
+                    <button onClick={() => { openLinkFn('https://youtu.be/dQw4w9WgXcQ?si=oH-CndaIq-3l8si3') }} className='link-btn' href="https://youtu.be/dQw4w9WgXcQ?si=oH-CndaIq-3l8si3">Free Money</button>
                     <button>Test Button</button>
+                    <button onClick={()=>{showMainBtnFn('close')}}>Show close button</button>
                 </div>
             </div>
         </>
